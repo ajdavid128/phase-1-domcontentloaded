204c22e99fab1785d4c2e9571919ba2c039f5bcb
Aaron David <ajdavid128@gmail.com>
2022-10-15 18:48:06 -0500

    commit tree 0d64051c6e94da4a710a8e514de2d978d98f1161

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', e => {changeText()});
+// console.log("This should log first")
+
+
+function changeText(){
+    document.querySelector('p').textContent = document.querySelector('p').textContent.replace('JavaScript is so cool. It lets me add text to my page programmatically.', 'This is really cool!')
+}
